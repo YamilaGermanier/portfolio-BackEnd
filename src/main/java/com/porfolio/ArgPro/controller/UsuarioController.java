401dc8bb95d93836a982f57c2dbc7575cec0e223
YamilaGermanier <yamiger91@gmail.com>
2023-05-09 16:59:08 -0300

    Agrego entity, service, repository y controller de Usuario

---
+++ src/main/java/com/porfolio/ArgPro/controller/UsuarioController.java
@@ -0,0 +1,55 @@
+package com.porfolio.ArgPro.controller;
+
+import com.porfolio.ArgPro.entity.Usuario;
+import com.porfolio.ArgPro.service.UsuarioService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users")
+@CrossOrigin(origins = "http://localhost:4200")
+public class UsuarioController {
+    
+    @Autowired
+    private UsuarioService userServ;
+    
+    @GetMapping("/lista")
+    @ResponseBody
+    public List<Usuario> verUsuario(){
+        return userServ.verUsuarios();
+    }
+    
+        @GetMapping("/ver/{id}")
+    @ResponseBody
+    public Usuario verPUsuario(@PathVariable int id){
+        return userServ.buscarUsuario(id);
+    }
+    
+    @PostMapping("/crear")
+    public String agregarUsuario(@RequestBody Usuario user){
+        userServ.crearUsuario(user);
+        return"LEl usuario fue creada correctamente";
+    }
+    
+    @DeleteMapping("/borrar/{id}")
+    public String eliminarUsiario(@PathVariable int id){
+       userServ.borrarUsuario(id);
+        return "El usuario fue borrado correctamente";
+}
+    @PutMapping("/editar")
+    public String modificarUsuario(@RequestBody Usuario user, @PathVariable Integer id){
+        userServ.editarUsuario(user, id);
+        return "Los cambios se guardaron correctamente.";
+    }
+    
+}
--- src/main/java/com/porfolio/ArgPro/repository/UsuarioRepository.java
+++ src/main/java/com/porfolio/ArgPro/repository/UsuarioRepository.java
@@ -1,9 +1,10 @@
 package com.porfolio.ArgPro.repository;
 
 import com.porfolio.ArgPro.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, integer>{
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
     
 }
